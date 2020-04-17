50bd7e28758880ca34b37d5c9f29f4bf37d266c5
Maykon A. Oliveira <moliveira.anschau@gmail.com>
2020-04-17 17:05:32 -0300

    Add user creation

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,37 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const existUser = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (existUser) {
+      throw new Error(`User with the email ${email} already exist`);
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
