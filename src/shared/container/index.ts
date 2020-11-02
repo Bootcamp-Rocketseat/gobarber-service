2d99da0e6e7b57ebdaa2adb2d5f796e8a12e3aab
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2020-11-02 20:55:21 -0300

    Added DI to UserTokensRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,9 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
 import '@modules/users/providers';
 import '@shared/providers';
 
@@ -13,7 +16,13 @@ container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
   AppointmentsRepository,
 );
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
