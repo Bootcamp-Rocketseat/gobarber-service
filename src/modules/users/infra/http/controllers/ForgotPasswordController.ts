7c89f91f4bcde51dc9b0056edcc50077004c966f
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2020-11-02 20:44:47 -0300

    add routes and controller to password services

---
+++ src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import SendEmailPasswordForgotService from '@modules/users/services/SendEmailPasswordForgotService';
+
+export default class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const sendEmailPasswordForgot = container.resolve(
+      SendEmailPasswordForgotService,
+    );
+
+    await sendEmailPasswordForgot.execute({
+      email,
+    });
+
+    return response.status(204);
+  }
+}
