af308dd69421af64335d2a444fb27f69e0783b4a
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2021-01-18 21:35:18 -0300

    Add invalidation for the providers list cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -6,6 +6,7 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IHashProvider from '@modules/users/providers/Hash/models/IHashProvider';
 
 import AppError from '@shared/errors/AppError';
+import ICacheProvider from '@shared/providers/Cache/models/ICacheProvider';
 
 interface IRequest {
   name: string;
@@ -21,6 +22,9 @@ class CreateUserService {
 
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -37,6 +41,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
