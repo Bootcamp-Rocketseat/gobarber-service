c83dda84fc27fa9bc6f5a4f015c8b5ee54a41c6c
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2021-01-18 23:52:41 -0300

    Added rateLimiter

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -12,8 +12,8 @@ export default {
 
   config: {
     redis: {
-      host: 'localhost',
-      port: 6379,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     },
   },
 } as ICacheConfig;
