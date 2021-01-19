f59700c7f2822cbac6ec476401a639e350d88569
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2021-01-18 21:23:24 -0300

    Add redis to cache information

--- src/shared/providers/index.ts
+++ src/shared/providers/index.ts
@@ -9,6 +9,9 @@ import IEmailProvider from '@shared/providers/Email/models/IEmailProvider';
 import HandlebarsEmailTemplateProvider from '@shared/providers/EmailTemplate/implementations/HandlebarsEmailTemplateProvider';
 import IEmailTemplateProvider from '@shared/providers/EmailTemplate/models/IEmailTemplateProvider';
 
+import RedisCacheProvider from '@shared/providers/Cache/implementations/RedisCacheProvider';
+import ICacheProvider from '@shared/providers/Cache/models/ICacheProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
@@ -22,4 +25,9 @@ container.registerSingleton<IEmailTemplateProvider>(
 container.registerInstance<IEmailProvider>(
   'EmailProvider',
   container.resolve(MailtrapEmailProvider),
+);
+
+container.registerInstance<ICacheProvider>(
+  'CacheProvider',
+  container.resolve(RedisCacheProvider),
 );
